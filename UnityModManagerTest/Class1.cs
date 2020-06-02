9a5a490d1016208ed5324e3deecc9bcf943c3e6c
Tahvohck <Tahvohck@gmail.com>
2020-06-02 04:11:18 -0400

    Example/playground mod for working with UMM

---
+++ UnityModManagerTest/Class1.cs
@@ -0,0 +1,85 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityModManagerNet;
+
+namespace UnityModManagerTest
+{
+    using ModEntry = UnityModManager.ModEntry;
+    using ModLogger = UnityModManager.ModEntry.ModLogger;
+
+    [EnableReloading]
+    public static class TestMod
+    {
+        static int idxFixedUpdate = 0;
+        static int idxUpdate = 0;
+
+        public static void Load(ModEntry entryData)
+        {
+            entryData.Logger.Log($"Loaded from {entryData.Path}. [{entryData.Assembly.FullName}]" +
+                $"\nCan be reloaded: {entryData.CanReload}");
+            entryData.OnToggle = Toggle;
+            entryData.OnUnload = Unload;
+            entryData.OnFixedUpdate = FixedUpdate;
+            entryData.OnUpdate = Update;
+            entryData.OnShowGUI = ShowGUI;
+            entryData.OnHideGUI = HideGUI;
+            entryData.OnSaveGUI = SaveGUI;
+            entryData.Logger.Log(GameManager.getInstance().getGameState().ToString());
+        }
+
+        private static void SaveGUI(ModEntry entryData)
+        {
+            if (idxUpdate >= 3) return;
+
+            entryData.Logger.Log($"GUI Save");
+        }
+
+        private static void HideGUI(ModEntry entryData)
+        {
+            if (idxUpdate >= 3) return;
+
+            entryData.Logger.Log($"GUI save");
+        }
+
+        private static void ShowGUI(ModEntry entryData)
+        {
+            if (idxUpdate >= 3) return;
+
+            entryData.Logger.Log($"GUI show");
+        }
+
+        private static void Update(ModEntry entryData, float tDelta)
+        {
+            if (idxUpdate >= 3) return;
+
+            entryData.Logger.Log($"Update thread: IDX {idxUpdate++}, arg2 {tDelta}");
+        }
+
+
+        private static void FixedUpdate(ModEntry entryData, float tDelta)
+        {
+            if (idxFixedUpdate >= 3) return;
+
+            entryData.Logger.Log($"FixedUpdate thread: IDX {idxFixedUpdate++}, arg2 {tDelta}");
+        }
+
+        /// <summary> An example of mod toggling. </summary>
+        /// <param name="onState">True: Enable mod, False: Disable Mod</param>
+        /// <returns>True if the toggle was successful.</returns>
+        public static bool Toggle(ModEntry entry, bool onState)
+        {
+            ModLogger logger = entry.Logger;
+            logger.Log($"Toggled {onState}");
+            return true;
+        }
+
+        public static bool Unload(ModEntry entry)
+        {
+            entry.Logger.Log("Unloading...");
+            return true;
+        }
+    }
+}
---
+++ UnityModManagerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityModManagerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnityModManagerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d3a0d0de-6b30-4eb6-8d68-4985b2ad2530")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
