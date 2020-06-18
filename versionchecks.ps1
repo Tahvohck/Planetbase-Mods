7f408574da06e334ef23727097e64289aaaa510c
Tahvohck <Tahvohck@gmail.com>
2020-06-17 21:24:06 -0400

    Touch up formatting.

--- versionchecks.ps1
+++ versionchecks.ps1
@@ -1,2 +1,2 @@
-write-host "GITD " (git describe --tags --always)
-write-host "JSON " (gc Properties/Info.json | ConvertFrom-Json | Select -Expand Version)
+write-host ("GITD  {0,-20} {1}" -f (git describe --tags --always),(git rev-parse --abbrev-ref HEAD))
+write-host ("JSON  {0}" -f (gc Properties/Info.json | ConvertFrom-Json | Select -Expand Version))
