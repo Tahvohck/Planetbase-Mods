ac7f7e965ef8ad624ca70dfb0093e3bd9d53e085
Tahvohck <Tahvohck@gmail.com>
2020-06-17 22:56:03 -0400

    Update submodule version utility for more clarity

--- versionchecks.ps1
+++ versionchecks.ps1
@@ -1,2 +1,8 @@
-write-host ("GITD  {0,-20} {1}" -f (git describe --tags --always),(git rev-parse --abbrev-ref HEAD))
-write-host ("JSON  {0}" -f (gc Properties/Info.json | ConvertFrom-Json | Select -Expand Version))
+$gitVer = git describe --tags --always
+$gitBranch = git rev-parse --abbrev-ref HEAD
+$jsonVer = [Version](gc Properties/Info.json | ConvertFrom-Json | Select -Expand Version)
+$VersionsMatch = if ($gitVer -like "$($jsonVer.ToString(3))*") { [char]8730 }
+
+write-host ("GITD  {0,-20} {1}" -f $gitVer,$gitBranch)
+write-host ("JSON  {0,-20} " -f $jsonVer) -nonewline
+write-host $VersionsMatch -Fore Green
