4c828d9c7dcf239416eeaf898449687d7b5bf185
Zemogiter <zumi999@vp.pl>
2023-01-25 17:40:16 +0100

    Update CharacterCam.cs

--- CharacterCam/CharacterCam.cs
+++ CharacterCam/CharacterCam.cs
@@ -1,53 +1,54 @@
-﻿using Planetbase;
+using HarmonyLib;
+using Planetbase;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using UnityEngine;
-using UnityModManagerNet;
-
+using static UnityModManagerNet.UnityModManager;
+using System.Reflection;
+using PlanetbaseModUtilities;
 
 namespace CharacterCam
 {
-    public class CharacterCam
+    public class CharacterCam : ModBase
     {
-        
-        [LoaderOptimization(LoaderOptimization.NotSpecified)]
-        public static void Init(UnityModManager.ModEntry modData)
-        {
-            modData.OnUpdate = Update;
+        public static new void Init(ModEntry modEntry) => InitializeMod(new CharacterCam(), modEntry, "CharacterCam");
+
+        public override void OnInitialized(ModEntry modEntry)
+		{
+            StringUtils.RegisterString("CharacterCam", "Character Cam");
             Debug.Log("[MOD] CharacterCam activated");
         }
 
-        public static void Update(UnityModManager.ModEntry modData, float timeStep) 
-        {
+		public override void OnUpdate(ModEntry modEntry, float timeStep)
+		{
             
         }
-    }
 
-    public abstract class CustomCharacter : Character
+    }
+    [HarmonyPatch(typeof(Character), nameof(Character.isCloseCameraAvailable))]
+    public class CharacterPatch
     {
-        public override bool isCloseCameraAvailable()
+        public static bool Prefix(ref bool __result)
         {
-            return true;
+            __result = true;
+            return false;
         }
     }
-
-    public abstract class CustomCloseCameraCinematic : CloseCameraCinematic
+    [HarmonyPatch(typeof(CloseCameraCinematic), nameof(CloseCameraCinematic.updateCharacter))]
+    public class CloseCameraCinematicPatch
     {
-        private CustomCloseCameraCinematic(Selectable s, bool b) : base(s, b) { }
-
-        public void UpdateCharacter(Character character, float timeStep)
+        private CloseCameraCinematicPatch(Selectable s, bool b) : base() { }
+        public static bool Prefix(Character character, float timeStep)
         {
+            
             Transform cameraTransform = CameraManager.getInstance().getTransform();
             Transform characterTransform = character.getTransform();
 
             double yAngle = characterTransform.eulerAngles.y;
-            FieldInfo fi = typeof(CloseCameraCinematic).GetField("mLastRotation");
-            double fiC = Convert.ToDouble(fi);
+            FieldInfo lastRotation = typeof(CloseCameraCinematic).GetField("mLastRotation");
+            double fiC = Convert.ToDouble(lastRotation);
             Debug.Log(fiC);
-            float horizontalBobbing = Mathf.Clamp((float)((Convert.ToDouble(fi) - yAngle) * 0.25f), -0.5f, 0.5f);
+            float horizontalBobbing = Mathf.Clamp((float)((Convert.ToDouble(lastRotation) - yAngle) * 0.25f), -0.5f, 0.5f);
             Vector3 newPos = characterTransform.position + Vector3.up * character.getHeight() + characterTransform.forward * 0.7f + horizontalBobbing * characterTransform.right;
             FieldInfo fi2 = typeof(CloseCameraCinematic).GetField("mFirstUpdate");
             Debug.Log(fi2);
@@ -58,12 +59,12 @@ namespace CharacterCam
                 fiC = yAngle;
                 Debug.Log(fiC);
             }
-
             cameraTransform.position = Vector3.Lerp(cameraTransform.position, newPos, 0.1f);
             Vector3 lookAtDir = (characterTransform.position + characterTransform.forward * 1.4f + Vector3.up * (character.getHeight() * 0.85f) - cameraTransform.position).normalized;
             cameraTransform.rotation = Quaternion.RotateTowards(cameraTransform.rotation, Quaternion.LookRotation(lookAtDir), timeStep * 120f);
             fiC = yAngle;
-            Debug.Log(fiC);
+
+            return false;
         }
     }
 }
